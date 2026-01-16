7fbb325ee365b987b3476c776c2eeb8fc9520045
YizhengQiao2002 <yizhengq@usc.edu>
2026-01-15 21:00:16 -0800

    Update client.c

--- lab1/client.c
+++ lab1/client.c
@@ -42,16 +42,28 @@ int main(int argc, char *argv[])
     serv_addr.sin_port = htons(portno);
     if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
         error("ERROR connecting");
-    printf("Please enter the message: ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);
-    n = write(sockfd,buffer,strlen(buffer));
-    if (n < 0) 
-         error("ERROR writing to socket");
-    bzero(buffer,256);
-    n = read(sockfd,buffer,255);
-    if (n < 0) 
-         error("ERROR reading from socket");
-    printf("%s\n",buffer);
+    while (1) {
+       printf("Please enter the message (Ctrl+D to quit): ");
+       bzero(buffer, 256);
+
+       if (fgets(buffer, 255, stdin) == NULL) {
+           printf("\nClient exiting\n");
+           break;
+       }
+
+       n = write(sockfd, buffer, strlen(buffer));
+       if (n < 0)
+          error("ERROR writing to socket");
+
+       bzero(buffer, 256);
+
+       n = read(sockfd, buffer, 255);
+       if (n <= 0) {
+           printf("Server closed connection\n");
+           break;
+        }
+        printf("Server reply: %s\n", buffer);
+     }
+
     return 0;
 }
